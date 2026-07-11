90732f2812dc179eb70e74b042a491418fbe86f7
ZachRootCAD <rootzach.cad@gmail.com>
2026-07-11 21:21:29 +0200

    Add Vercel and production domains to CORS

--- api/main.py
+++ api/main.py
@@ -12,10 +12,12 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+       "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "https://graffitiatlas.io"
+        "https://graffiti-atlas.vercel.app",
+        "https://graffitiatlas.io",
+        "https://www.graffitiatlas.io"
     ],
     allow_credentials=True,
     allow_methods=["*"],
